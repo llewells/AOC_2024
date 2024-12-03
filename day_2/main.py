c3cc9af5bb6007e7d997ce3b94b2600f1f850b1c
Simon Llewellyn <llewells86@gmail.com>
2024-12-03 21:45:13 +0000

    unfinished day2

--- day_2/main.py
+++ day_2/main.py
@@ -8,54 +8,57 @@ import os
 os.chdir(os.path.dirname(__file__))
 TEST_FILE = "test.txt"
 DATA_FILE = "data.txt"
-filename = DATA_FILE 
+filename = DATA_FILE
 with open(filename) as f:
     data = f.read().splitlines()
 
-split_lines = [line.split(' ') for line in data]
+split_lines = [line.split(" ") for line in data]
 
 LIMIT = 3
 count = 0
 
-def check_line(line): 
 
-    asscending = None
+def check_line(line):
+
+    direction = None
 
     for i, item in enumerate(line[1:]):
         current = int(item)
         previous = int(line[i])
 
-        c_gt = (current >= previous)
         if current == previous:
-            return False, i         
+            return False
 
         if abs(current - previous) > LIMIT:
-            return False, i 
-        if asscending is None:
-            asscending = c_gt 
+            return False
+
+        is_accending = current > previous
+
+        if direction is None:
+            direction = is_accending
             continue
 
-        if asscending != c_gt:
-            return False, i
+        if is_accending != direction:
+            return False
 
-    return True, None
+    return True
+
 
 for line in split_lines:
-    print(count)
-    result, idx = check_line(line)
+    print("-" * 20)
+    result = check_line(line)
     print(result, line)
     if result:
         count += 1
         continue
 
-    for i in range(2):
+    for i in range(len(line)):
         _line = line.copy()
-        del _line[idx + i]
-        result, _ = check_line(_line)
+        del _line[i]
+        result = check_line(_line)
         print(result, _line)
         if result:
             count += 1
             break
 
-print([len(l) for l in split_lines])
 print(count)
